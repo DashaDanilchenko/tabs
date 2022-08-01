8125f6ba4e703b31ee6185b91f15f017bd1fe601
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-08-01 11:05:33 +0300

    function activeTab

--- script.js
+++ script.js
@@ -11,13 +11,59 @@ contents.classList.add('contents')
 container.append(contents)
 
 tabs.innerHTML = `
-<div class="javaScript design">Java Script</div>
+<div class="javaScript design active">Java Script</div>
 <div class="ruby design">Ruby</div>
 <div class="pyp design">PHP</div>
 `
 
 contents.innerHTML = `
-<div class="javaScript display">Java Script is great</div>
-<div class="ruby display">Ruby great</div>
-<div class="pyp display">PHP great</div>
+<div class="javaScript display active">Java Script is great!</div>
+<div class="ruby display">Ruby great!</div>
+<div class="pyp display">PHP great!</div>
 `
+
+const javaScript = document.querySelectorAll('.javaScript')
+const ruby = document.querySelectorAll('.ruby')
+const pyp = document.querySelectorAll('.pyp')
+
+tabs.addEventListener('click', activeTab)
+
+function activeTab (event) {
+    if (event.target.closest('.ruby')) {
+        ruby.forEach((i) => {
+            i.classList.add('active')
+        }) 
+        javaScript.forEach((i) => {
+            i.classList.remove('active')
+        }) 
+        pyp.forEach((i) => {
+            i.classList.remove('active')
+        })
+    }
+
+    if (event.target.closest('.pyp')) {
+        pyp.forEach((i) => {
+            i.classList.add('active')
+        }) 
+        javaScript.forEach((i) => {
+            i.classList.remove('active')
+        }) 
+        ruby.forEach((i) => {
+            i.classList.remove('active')
+        })
+    }
+
+    if (event.target.closest('.javaScript')) {
+        javaScript.forEach((i) => {
+            i.classList.add('active')
+        }) 
+        pyp.forEach((i) => {
+            i.classList.remove('active')
+        }) 
+        ruby.forEach((i) => {
+            i.classList.remove('active')
+        })
+    }
+
+
+}
